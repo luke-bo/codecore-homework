b9160700d51912f935c91f6785fbb9f600d1ca52
luke.bohonis <w.bohonis@gmail.com>
2019-09-26 17:07:48 -0700

    add user input with process.argv

--- box-it-script-week-1/box-it.js
+++ box-it-script-week-1/box-it.js
@@ -1,9 +1,22 @@
-function drawLine(lineLength) {
-    return '━'.repeat(lineLength);
+// longestStringInArray helper function to determine the width of the box, based on the longest string in the array of strings passed to the boxIt function
+
+function longestStringInArray(arr) {
+    let maxStringLength = 0 
+    for (let element of arr) {
+        if (element.length > maxStringLength) {
+            maxStringLength = element.length;
+        }
+    }
+    return maxStringLength;
 }
 
 
 
+// functions to be used within the main boxIt function
+
+function drawLine(lineLength) {
+    return '━'.repeat(lineLength);
+}
 
 function drawTopBorder(topBorderLength) {
     return '┏' + drawLine(topBorderLength) + `┓\n`;
@@ -17,49 +30,57 @@ function drawBottomBorder(bottomBorderLength) {
     return '┗' + drawLine(bottomBorderLength) + '┛';
 }
 
-function drawBarsAround(string) {
-    return '┃' + string + `┃\n`;
+function drawBarsAround(string, width) {
+    return '┃' + string + ' '.repeat((width - string.length)) + `┃\n`;
 }
 
-function longestStringInArray(arr) {
-    let maxStringLength = 0 
-    for (let element of arr) {
-        if (element.length > maxStringLength) {
-            maxStringLength = element.length;
-        }
-    }
-    return maxStringLength;
-}
 
-function boxIt(ArrOfStrings) {
-    // console.log(strArr);
+
+function boxIt(arrOfStrings) {
+// console.log(strArr);
     let boxedOutput = '';
-    let widthOfBox = longestStringInArray(ArrOfStrings);
-    // console.log(ArrOfStrings);
+    let widthOfBox = longestStringInArray(arrOfStrings);
+    // console.log(arrOfStrings);
     // console.log(longestStringInArray);
     boxedOutput += drawTopBorder(widthOfBox);
 
-    if (ArrOfStrings.length == 0) {
+    if (arrOfStrings.length == 0) {
         return `┏┓\n`+`┗┛`;
     }
 
-    for (let str of ArrOfStrings) {
-        if (ArrOfStrings.indexOf(str) < ArrOfStrings.length-1) {
-            boxedOutput += drawBarsAround(str) + drawMiddleBorder(widthOfBox);
+    for (let str of arrOfStrings) {
+        if (arrOfStrings.indexOf(str) < arrOfStrings.length-1) {
+            boxedOutput += drawBarsAround(str, widthOfBox) + drawMiddleBorder(widthOfBox);
             
 
         } else {
-            boxedOutput += drawBarsAround(str) + drawBottomBorder(widthOfBox);
+            boxedOutput += drawBarsAround(str, widthOfBox) + drawBottomBorder(widthOfBox);
         }
     }
     console.log(boxedOutput);
-
 }
+
+
+// tests for individual functions
 
 // console.log(drawLine(5));
 // console.log(drawTopBorder(0));
 // console.log(drawMiddleBorder(4));
 // console.log(drawBottomBorder(4));
 // console.log(drawBarsAround('hello world!'));
-console.log(boxIt(['hello', 'world']));
-console.log(boxIt([]));
+
+
+// tests with pre-defined boxIt arguments
+
+// console.log(boxIt(['hello', 'world', 'cersei lanister']));
+// console.log(boxIt([]));
+// console.log(boxIt(['Jon Snow']));
+
+
+
+// boxIt w/ user input
+
+let arrOfStrings = process.argv.slice(2);  
+boxIt(arrOfStrings);
+
+
